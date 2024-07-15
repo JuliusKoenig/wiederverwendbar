61c5bac42c55235dc1c6d4e30597e4acdd5e6e12
Julius Koenig <julius@koenig-site.de>
2024-07-15 18:25:37 +0200

    v0.2.23   - reorg internal package structure starlette_admin   - add PropertyModelView

--- src/wiederverwendbar/starlette_admin/mongoengine/__init__.py
+++ src/wiederverwendbar/starlette_admin/mongoengine/__init__.py
@@ -1,8 +1,8 @@
 from wiederverwendbar.starlette_admin.mongoengine.admin import Admin
 
-from wiederverwendbar.starlette_admin.mongoengine.view.fixed_model_view import FixedModelView
+from wiederverwendbar.starlette_admin.mongoengine.view import FixedModelView
 
 from wiederverwendbar.starlette_admin.mongoengine.generic_embedded_document_field import GenericEmbeddedConverter
 from wiederverwendbar.starlette_admin.mongoengine.generic_embedded_document_field import GenericEmbeddedDocumentField
 
-from wiederverwendbar.starlette_admin.mongoengine.ipv4_field.converter import IPv4Converter
+from wiederverwendbar.starlette_admin.mongoengine.ipv4_field import IPv4Converter
