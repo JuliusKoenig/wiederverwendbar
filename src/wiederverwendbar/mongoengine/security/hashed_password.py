bd2feb07587cfade8aeef0e51e5f748e49f52628
Julius Koenig <julius@koenig-site.de>
2024-08-09 15:44:49 +0200

    chg: fix hashed_password.py !minor

--- src/wiederverwendbar/mongoengine/security/hashed_password.py
+++ src/wiederverwendbar/mongoengine/security/hashed_password.py
@@ -5,6 +5,8 @@ from wiederverwendbar.functions.security.hashed_password import HashedPassword
 
 
 class HashedPasswordDocument(EmbeddedDocument, HashedPassword):
+    meta = {"allow_inheritance": True}
+
     encoding: str = StringField(required=True)
     hash_function: str = StringField(required=True)
     interactions: int = IntField(required=True)
