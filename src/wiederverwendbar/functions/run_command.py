8521c13758de8d7a8c1dc46721383b7e1f94eac5
Julius Koenig <julius@koenig-site.de>
2025-03-27 16:45:26 +0100

    Enhance run_command function with customizable encoding.
    Added optional parameters for output encoding and error handling. Default encoding behavior now adapts to the platform, improving cross-platform compatibility and flexibility for handling command output.

--- src/wiederverwendbar/functions/run_command.py
+++ src/wiederverwendbar/functions/run_command.py
@@ -1,17 +1,32 @@
 import logging
 import subprocess
+import sys
+from typing import Optional
+
+from typing_extensions import Literal
 
 logger = logging.getLogger(__name__)
 
 
-def run_command(cmd: list[str], expected_exit_code=0) -> tuple[bool, list[str], list[str]]:
+def run_command(cmd: list[str],
+                expected_exit_code: int = 0,
+                encoding: Optional[str] = None,
+                encoding_errors: Literal["strict", "ignore", "replace"] = "strict") -> tuple[bool, list[str], list[str]]:
     """
     Run a command and log its output.
 
     :param cmd: Command as list of strings
     :param expected_exit_code: Expected exit code of command
+    :param encoding: Encoding of command output
+    :param encoding_errors: Encoding error handling
     :return: True if command was successful, False otherwise
     """
+
+    if encoding is None:
+        if sys.platform == "win32":
+            encoding = "cp437"
+        else:
+            encoding = "utf-8"
 
     cmd_str = " ".join(cmd)
     logger.debug(f"Run command: {cmd_str}")
@@ -24,12 +39,12 @@ def run_command(cmd: list[str], expected_exit_code=0) -> tuple[bool, list[str], 
     stderr = []
     with process.stdout and process.stderr:
         for line in iter(process.stdout.readline, b''):
-            line_str = line.decode("cp437").strip()
+            line_str = line.decode(encoding=encoding, errors=encoding_errors).strip()
             logger.debug("stdout: " + line_str)
             stdout.append(line_str)
 
         for line in iter(process.stderr.readline, b''):
-            line_str = line.decode("cp437").strip()
+            line_str = line.decode(encoding=encoding, errors=encoding_errors).strip()
             logger.debug("stderr: " + line_str)
             stderr.append(line_str)
 
