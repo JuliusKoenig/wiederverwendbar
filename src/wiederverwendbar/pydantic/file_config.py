df89e1f9490f09e3643ed49055942e0e384a4b91
Julius Koenig <julius@koenig-site.de>
2025-06-04 19:09:53 +0200

    Add flexible handling for `file_must_exist` in `FileConfig` with new behavior options
    Enhanced `FileConfig` to support extended `file_must_exist` options (`yes_print`, `yes_warn`, `yes_raise`, `no`), providing customizable responses when a file is not found.

--- src/wiederverwendbar/pydantic/file_config.py
+++ src/wiederverwendbar/pydantic/file_config.py
@@ -1,15 +1,19 @@
 import json
+import sys
+from warnings import warn
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, Union, Literal
 
 from pydantic import BaseModel, create_model
+
+from wiederverwendbar.warnings import FileNotFoundWarning
 
 
 class FileConfig(BaseModel):
     def __init__(self,
                  file_path: Union[Path, str, None] = None,
                  file_postfix: str = ".json",
-                 file_must_exist: bool = False,
+                 file_must_exist: Union[bool, Literal["yes_print", "yes_warn", "yes_raise", "no"]] = "no",
                  **overwrite_data: Any):
         if file_path is None:
             file_path = Path(Path.cwd() / self.__class__.__name__.lower()).with_suffix(file_postfix)
@@ -25,8 +29,19 @@ class FileConfig(BaseModel):
         elif file_path.is_dir():
             raise ValueError(f"{self.__class__.__name__} file path '{file_path}' is a directory.")
         else:
-            if file_must_exist:
-                raise FileNotFoundError(f"{self.__class__.__name__} file '{file_path}' not found.")
+            if file_must_exist is True:
+                file_must_exist = "yes_raise"
+            elif file_must_exist is False:
+                file_must_exist = "no"
+            msg = f"{self.__class__.__name__} file '{file_path}' not found."
+            if file_must_exist == "yes_print":
+                print(msg)
+                sys.exit(1)
+            elif file_must_exist == "yes_warn":
+                warn(msg, FileNotFoundWarning)
+                sys.exit(1)
+            elif file_must_exist == "yes_raise":
+                raise FileNotFoundError(msg)
             data = {}
 
         # overwrite data
