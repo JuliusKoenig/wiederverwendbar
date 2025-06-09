b7b821bef06c7545c6851d17506d06ca568fa79d
Julius Koenig <julius@koenig-site.de>
2025-06-09 18:47:55 +0200

    Refactor `Console` and `RichConsole` classes for streamlined initialization, enhanced configurability, and support for filtered print methods.

--- src/wiederverwendbar/console/console.py
+++ src/wiederverwendbar/console/console.py
@@ -1,18 +1,54 @@
-import sys
-from abc import ABC, abstractmethod
 from typing import Optional, Any, Literal, Union
 
 from wiederverwendbar.console.out_files import OutFiles
 from wiederverwendbar.console.settings import ConsoleSettings
 
 
-class BaseConsole(ABC):
+class Console:
+    print_function = print
+    print_function_blacklist_kwargs = ["file"]
     console_border_styles = {
         "single_line": ["─", "│", "┌", "┐", "└", "┘", "├", "┤"],
         "double_line": ["═", "║", "╔", "╗", "╚", "╝", "╠", "╣"]
     }
 
-    @abstractmethod
+    def __init__(self,
+                 *,
+                 console_file: Optional[OutFiles] = None,
+                 console_seperator: Optional[str] = None,
+                 console_end: Optional[str] = None,
+                 settings: Optional[ConsoleSettings] = None):
+        """
+        Create a new console.
+
+        :param console_file: Console file. Default is STDOUT.
+        :param console_seperator: Console seperator. Default is a space.
+        :param console_end: Console end. Default is a newline.
+        :param settings: A settings object to use. If None, defaults to ConsoleSettings().
+        """
+
+        if settings is None:
+            settings = ConsoleSettings()
+
+        if console_file is None:
+            console_file = settings.console_file
+        self._console_file = console_file
+
+        if console_seperator is None:
+            console_seperator = settings.console_seperator
+        self._console_seperator = console_seperator
+
+        if console_end is None:
+            console_end = settings.console_end
+        self._console_end = console_end
+
+    def _print_method_kwargs_filter(self, **kwargs) -> dict[str, Any]:
+        for key in kwargs.copy():
+            if key not in self.print_function_blacklist_kwargs:
+                continue
+            del kwargs[key]
+        return kwargs
+
     def print(self,
               *args: Any,
               sep: Optional[str] = None,
@@ -26,6 +62,12 @@ class BaseConsole(ABC):
         :param end:  string appended after the last value, default a newline.
         :param kwargs: Additional parameters.
         """
+
+        if sep is None:
+            sep = self._console_seperator
+        if end is None:
+            end = self._console_end
+        self.print_function(*args, **self._print_method_kwargs_filter(sep=sep, end=end, file=OutFiles.STDOUT.get_file(), **kwargs))
 
     def _card_kwargs(self, mode: Literal["text", "header", "border", "print"], **kwargs) -> dict[str, Any]:
         return {}
@@ -134,78 +176,24 @@ class BaseConsole(ABC):
 
                 formatted_lines.append(line)
             formatted_sections.append(formatted_lines)
-        card = (f"\n{self._card_get_border(border_style, 'top_left', **self._card_kwargs('border', **kwargs))}"
-                f"{self._card_get_border(border_style, 'horizontal', **self._card_kwargs('border', **kwargs)) * topic_offest}"
-                f"{self._card_get_header_text(section_topics[0], **self._card_kwargs('header', **kwargs))}"
-                f"{self._card_get_border(border_style, 'horizontal', **self._card_kwargs('border', **kwargs)) * (real_width - len(section_topics.pop(0)) - topic_offest)}"
-                f"{self._card_get_border(border_style, 'top_right', **self._card_kwargs('border', **kwargs))}\n")
+        card = (f"\n{self._card_get_border(border_style, 'top_left', **kwargs)}"
+                f"{self._card_get_border(border_style, 'horizontal', **kwargs) * topic_offest}"
+                f"{self._card_get_header_text(section_topics[0], **kwargs)}"
+                f"{self._card_get_border(border_style, 'horizontal', **kwargs) * (real_width - len(section_topics.pop(0)) - topic_offest)}"
+                f"{self._card_get_border(border_style, 'top_right', **kwargs)}\n")
         while len(formatted_sections) > 0:
             for line in formatted_sections.pop(0):
-                card += (f"{self._card_get_border(border_style, 'vertical', **self._card_kwargs('border', **kwargs))}"
-                         f"{self._card_get_text(line, **self._card_kwargs('text', **kwargs))}"
-                         f"{self._card_get_border(border_style, 'vertical', **self._card_kwargs('border', **kwargs))}\n")
+                card += (f"{self._card_get_border(border_style, 'vertical', **kwargs)}"
+                         f"{self._card_get_text(line, **kwargs)}"
+                         f"{self._card_get_border(border_style, 'vertical', **kwargs)}\n")
             if len(formatted_sections) > 0:
-                card += (f"{self._card_get_border(border_style, 'vertical_left', **self._card_kwargs('border', **kwargs))}"
-                         f"{self._card_get_border(border_style, 'horizontal', **self._card_kwargs('border', **kwargs)) * topic_offest}"
-                         f"{self._card_get_header_text(section_topics[0], **self._card_kwargs('header', **kwargs))}"
-                         f"{self._card_get_border(border_style, 'horizontal', **self._card_kwargs('border', **kwargs)) * (real_width - len(section_topics.pop(0)) - topic_offest)}"
-                         f"{self._card_get_border(border_style, 'vertical_right', **self._card_kwargs('border', **kwargs))}\n")
+                card += (f"{self._card_get_border(border_style, 'vertical_left', **kwargs)}"
+                         f"{self._card_get_border(border_style, 'horizontal', **kwargs) * topic_offest}"
+                         f"{self._card_get_header_text(section_topics[0], **kwargs)}"
+                         f"{self._card_get_border(border_style, 'horizontal', **kwargs) * (real_width - len(section_topics.pop(0)) - topic_offest)}"
+                         f"{self._card_get_border(border_style, 'vertical_right', **kwargs)}\n")
             else:
-                card += (f"{self._card_get_border(border_style, 'bottom_left', **self._card_kwargs('border', **kwargs))}"
-                         f"{self._card_get_border(border_style, 'horizontal', **self._card_kwargs('border', **kwargs)) * real_width}"
-                         f"{self._card_get_border(border_style, 'bottom_right', **self._card_kwargs('border', **kwargs))}\n")
-        return self.print(card, **self._card_kwargs('print', **kwargs))
-
-
-class Console(BaseConsole):
-    def __init__(self,
-                 *,
-                 console_file: Optional[OutFiles] = None,
-                 console_seperator: Optional[str] = None,
-                 console_end: Optional[str] = None,
-                 settings: Optional[ConsoleSettings] = None):
-        """
-        Create a new console.
-
-        :param console_file: Console file. Default is STDOUT.
-        :param console_seperator: Console seperator. Default is a space.
-        :param console_end: Console end. Default is a newline.
-        :param settings: A settings object to use. If None, defaults to ConsoleSettings().
-        """
-
-        if settings is None:
-            settings = ConsoleSettings()
-        if console_file is None:
-            console_file = settings.console_file
-        self.console_file = console_file
-        if console_seperator is None:
-            console_seperator = settings.console_seperator
-        self.console_seperator = console_seperator
-        if console_end is None:
-            console_end = settings.console_end
-        self.console_end = console_end
-
-    def print(self,
-              *args: Any,
-              sep: Optional[str] = None,
-              end: Optional[str] = None,
-              **kwargs) -> None:
-        """
-        Prints the values.
-
-        :param args: values to be printed.
-        :param sep:  string inserted between values, default a space.
-        :param end:  string appended after the last value, default a newline.
-        :param kwargs: Additional parameters.
-        """
-
-        if sep is None:
-            sep = self.console_seperator
-        if end is None:
-            end = self.console_end
-        if self.console_file == OutFiles.STDOUT:
-            print(*args, sep=sep, end=end, file=sys.stdout, **kwargs)
-        elif self.console_file == OutFiles.STDERR:
-            print(*args, sep=sep, end=end, file=sys.stderr, **kwargs)
-        else:
-            raise ValueError(f"Unknown console file '{self.console_file}'.")
+                card += (f"{self._card_get_border(border_style, 'bottom_left', **kwargs)}"
+                         f"{self._card_get_border(border_style, 'horizontal', **kwargs) * real_width}"
+                         f"{self._card_get_border(border_style, 'bottom_right', **kwargs)}\n")
+        return self.print(card, **kwargs)
--- src/wiederverwendbar/rich/console.py
+++ src/wiederverwendbar/rich/console.py
@@ -11,6 +11,9 @@ from wiederverwendbar.rich.settings import RichConsoleSettings
 
 
 class RichConsole(_Console, _RichConsole):
+    print_function = _RichConsole.print
+    print_function_blacklist_kwargs = _Console.print_function_blacklist_kwargs + ["color", "header_color", "border_color"]
+
     def __init__(self,
                  *,
                  console_file: Optional[OutFiles] = None,
@@ -36,8 +39,7 @@ class RichConsole(_Console, _RichConsole):
                  console_log_time: Optional[bool] = None,
                  console_log_path: Optional[bool] = None,
                  console_log_time_format: Union[str, FormatTimeCallable] = "[%X]",  # ToDo: not in settings included
-                 settings: Optional[RichConsoleSettings] = None,
-                 **kwargs):
+                 settings: Optional[RichConsoleSettings] = None):
         """
         Create a new rich console.
 
@@ -49,68 +51,72 @@ class RichConsole(_Console, _RichConsole):
 
         if settings is None:
             settings = RichConsoleSettings()
-        _Console.__init__(self, console_file=console_file, console_seperator=console_seperator, console_end=console_end, settings=settings)
+
+        _Console.__init__(self,
+                          console_file=console_file,
+                          console_seperator=console_seperator,
+                          console_end=console_end,
+                          settings=settings)
 
         if console_color_system is None:
             console_color_system = settings.console_color_system
-        self.console_color_system = console_color_system
+
         if console_force_terminal is None:
             console_force_terminal = settings.console_force_terminal
-        self.console_force_terminal = console_force_terminal
+
         if console_force_jupyter is None:
             console_force_jupyter = settings.console_force_jupyter
-        self.console_force_jupyter = console_force_jupyter
+
         if console_force_interactive is None:
             console_force_interactive = settings.console_force_interactive
-        self.console_force_interactive = console_force_interactive
+
         if console_soft_wrap is None:
             console_soft_wrap = settings.console_soft_wrap
-        self.console_soft_wrap = console_soft_wrap
+
         # if console_theme is None: # ToDo: not in settings included
         #     console_theme = settings.console_theme
-        # self.console_theme = console_theme # ToDo: not in settings included
+
         if console_quiet is None:
             console_quiet = settings.console_quiet
-        self.console_quiet = console_quiet
+
         if console_width is None:
             console_width = settings.console_width
-        self.console_width = console_width
+
         if console_height is None:
             console_height = settings.console_height
-        self.console_height = console_height
+
         # if console_style is None: # ToDo: not in settings included
         #     console_style = settings.console_style # ToDo: not in settings included
-        # self.console_style = console_style # ToDo: not in settings included
+
         if console_no_color is None:
             console_no_color = settings.console_no_color
-        self.console_no_color = console_no_color
+
         if console_tab_size is None:
             console_tab_size = settings.console_tab_size
-        self.console_tab_size = console_tab_size
+
         if console_record is None:
             console_record = settings.console_record
-        self.console_record = console_record
+
         if console_markup is None:
             console_markup = settings.console_markup
-        self.console_markup = console_markup
+
         if console_emoji is None:
             console_emoji = settings.console_emoji
-        self.console_emoji = console_emoji
+
         if console_emoji_variant is None:
             console_emoji_variant = settings.console_emoji_variant
-        self.console_emoji_variant = console_emoji_variant
+
         if console_highlight is None:
             console_highlight = settings.console_highlight
-        self.console_highlight = console_highlight
+
         if console_log_time is None:
             console_log_time = settings.console_log_time
-        self.console_log_time = console_log_time
+
         if console_log_path is None:
             console_log_path = settings.console_log_path
-        self.console_log_path = console_log_path
+
         # if console_log_time_format is None: # ToDo: not in settings included
         #     console_log_time_format = settings.console_log_time_format # ToDo: not in settings included
-        # self.console_log_time_format = console_log_time_format # ToDo: not in settings included
 
         _RichConsole.__init__(self,
                               color_system=console_color_system,
@@ -133,26 +139,6 @@ class RichConsole(_Console, _RichConsole):
                               log_time=console_log_time,
                               log_path=console_log_path,
                               log_time_format=console_log_time_format)
-
-    def print(self,
-              *args: Any,
-              sep: Optional[str] = None,
-              end: Optional[str] = None,
-              **kwargs) -> None:
-        """
-        Prints the values.
-
-        :param args: values to be printed.
-        :param sep:  string inserted between values, default a space.
-        :param end:  string appended after the last value, default a newline.
-        :param kwargs: Additional parameters.
-        """
-
-        if sep is None:
-            sep = self.console_seperator
-        if end is None:
-            end = self.console_end
-        _RichConsole.print(self, *args, sep=sep, end=end, **kwargs)
 
     def _card_kwargs(self, mode: Literal["text", "header", "border", "print"], **kwargs) -> dict[str, Any]:
         out = super()._card_kwargs(mode=mode, **kwargs)
