ea54841a8b4325eccb2a74c39b75a252e5dc4ab7
Julius Koenig <julius@koenig-site.de>
2025-06-10 11:57:53 +0200

    Add `typer_branding.py` example showcasing `TyperBrandingApp` and `TyperBrandingSettings` usage.

---
+++ examples/typer_branding.py
@@ -0,0 +1,26 @@
+from wiederverwendbar import __author__, __author_email__, __license__, __license_url__, __terms_of_service__
+from wiederverwendbar.typer import TyperBrandingSettings, TyperBrandingApp
+
+settings = TyperBrandingSettings(branding_title="Test App",
+                                 branding_description="Test Description",
+                                 branding_version="0.1.0",
+                                 branding_author=__author__,
+                                 branding_author_email=__author_email__,
+                                 branding_license=__license__,
+                                 branding_license_url=__license_url__,
+                                 branding_terms_of_service=__terms_of_service__)
+app = TyperBrandingApp(settings=settings)
+
+
+@app.command()
+def test1():
+    app.console.print("test1")
+
+
+@app.command()
+def test2():
+    app.console.print("test2")
+
+
+if __name__ == "__main__":
+    app()
