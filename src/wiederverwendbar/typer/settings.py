48d50eaf3529dff1a5ec17596821aca6391ca1bf
Julius Koenig <julius@koenig-site.de>
2025-06-10 12:13:23 +0200

    Refactor `TyperSettings` to enhance initialization by setting defaults for `cli_name` and `cli_help`.

--- src/wiederverwendbar/typer/settings.py
+++ src/wiederverwendbar/typer/settings.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any
 
 from pydantic import Field
 
@@ -8,3 +8,12 @@ from wiederverwendbar.rich import RichConsoleSettings
 class TyperSettings(RichConsoleSettings):
     cli_name: Optional[str] = Field(default=None, title="CLI Name", description="The name of the CLI.")
     cli_help: Optional[str] = Field(default=None, title="CLI Help", description="The help of the CLI.")
+
+    def model_post_init(self, context: Any, /):
+        super().model_post_init(context)
+
+        if self.cli_name is None:
+            self.cli_name = self.branding_title
+
+        if self.cli_help is None:
+            self.cli_help = self.branding_description
