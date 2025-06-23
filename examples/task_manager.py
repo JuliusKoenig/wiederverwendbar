5aee417c3e48fbfc72f2ede637cb0c57cb1d03a8
Julius Koenig <julius@koenig-site.de>
2025-06-23 11:08:32 +0200

    Disable `Manager2` usage in `task_manager` example for simplification

--- examples/task_manager.py
+++ examples/task_manager.py
@@ -10,7 +10,7 @@ logger.addHandler(ch)
 
 # create manager
 manager1 = TaskManager(name="Manager1", logger=logger)
-manager2 = TaskManager(name="Manager2", logger=logger)
+# manager2 = TaskManager(name="Manager2", logger=logger)
 
 
 @manager1.task(name="Task 1", trigger=AtCreation(delay_for_seconds=10))
@@ -21,13 +21,13 @@ def task1():
 @manager1.task(name="Task 2", trigger=EverySeconds(2))
 def task2():
     logger.debug("Task 2 ...")
-    time.sleep(2)
+    # time.sleep(2)
 
 
 @manager1.task(name="Task 3", trigger=EverySeconds(4))
 def task3():
     logger.debug("Task 3 ...")
-    time.sleep(4)
+    # time.sleep(4)
 
 
 if __name__ == '__main__':
@@ -35,21 +35,23 @@ if __name__ == '__main__':
 
     # start worker
     manager1.start()
-    manager2.start()
+    # manager2.start()
 
     # create tasks
-    Task(name="Task 1", manager=manager2, trigger=AtCreation(delay_for_seconds=10), payload=task1)
-    Task(name="Task 2", manager=manager2, trigger=EverySeconds(2), payload=task2)
-    Task(name="Task 3", manager=manager2, trigger=EverySeconds(4), payload=task3)
+    # Task(name="Task 1", manager=manager2, trigger=AtCreation(delay_for_seconds=10), payload=task1)
+    # Task(name="Task 2", manager=manager2, trigger=EverySeconds(2), payload=task2)
+    # Task(name="Task 3", manager=manager2, trigger=EverySeconds(4), payload=task3)
 
     # enter main loop
     try:
         while True:
             logger.debug("Main loop ...")
-            # Manager().loop()
+            # if worker count is 0
+            # manager1.loop(stay_in_loop=False)
+            # manager2.loop(stay_in_loop=False)
             time.sleep(1)
     except KeyboardInterrupt:
         logger.debug("Keyboard interrupt.")
 
     manager1.stop()
-    manager2.stop()
+    # manager2.stop()
