9e318c24c8f5b45fe939219fee7e53055e3f7606
Julius Koenig <julius@koenig-site.de>
2025-06-23 17:15:40 +0200

    Update `task_manager` example to showcase new trigger capabilities
    - Replaced old triggers with updated class-based triggers (`AtManagerCreation`, `AtManagerStart`, `AtNow`, etc.).
    - Added `MyTrigger` implementation as a custom trigger example.
    - Refactored tasks to demonstrate advanced scheduling options.
    - Streamlined `Manager2` usage with simplified task definitions.

--- examples/task_manager.py
+++ examples/task_manager.py
@@ -1,32 +1,50 @@
 import logging
 import time
 
-from wiederverwendbar.task_manger import TaskManager, Task, AtCreation, EverySeconds
+from wiederverwendbar.task_manger import TaskManager, Task, AtManagerCreation, EverySeconds, Trigger, At, AtManagerStart, AtNow, EveryMinutes
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 logger.addHandler(ch)
 
+
+# create custom trigger
+class MyTrigger(Trigger):
+    def __init__(self):
+        super().__init__()
+
+        self.triggered = False
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(triggered={self.triggered})"
+
+    def check(self) -> bool:
+        if self.triggered:
+            return False
+        self.triggered = True
+        return True
+
+
 # create manager
 manager1 = TaskManager(name="Manager1", logger=logger)
 manager2 = TaskManager(name="Manager2", logger=logger)
 
 
-@manager1.task(name="Task 1", trigger=AtCreation(delay_for_seconds=10))
+@manager1.task(AtManagerCreation(delay_for_seconds=10))
 def task1():
-    logger.debug("Task 1 ...")
+    print("Manager1.Task1 ...")
 
 
-@manager1.task(name="Task 2", trigger=EverySeconds(2))
+@manager1.task(EverySeconds(2))
 def task2():
-    logger.debug("Task 2 ...")
+    print("Manager1.Task2 ...")
     time.sleep(2)
 
 
-@manager1.task(name="Task 3", trigger=EverySeconds(4))
+@manager1.task(At(second=0), At(second=10), At(second=20), At(second=30), At(second=40), At(second=50))
 def task3():
-    logger.debug("Task 3 ...")
+    print("Manager1.Task3 ...")
     time.sleep(4)
 
 
@@ -38,11 +56,11 @@ if __name__ == '__main__':
     manager2.start()
 
     # create tasks
-    Task(name="Task 1", manager=manager2, trigger=AtCreation(delay_for_seconds=10), payload=task1)
-    Task(name="Task 2", manager=manager2, trigger=EverySeconds(2), payload=task2)
-    Task(name="Task 3", manager=manager2, trigger=EverySeconds(4), payload=task3)
+    manager2.add_task(Task(lambda: print("Manager2.Task1 ..."), AtManagerStart(delay_for_seconds=5)))
+    manager2.add_task(Task(lambda: print("Manager2.Task2 ..."), AtNow(delay_for_minutes=1)))
+    manager2.add_task(Task(lambda: print("Manager2.Task3 ..."), EveryMinutes(1)))
 
-    # enter main loop
+    # enter the main loop
     try:
         while True:
             logger.debug("Main loop ...")
@@ -54,4 +72,4 @@ if __name__ == '__main__':
         logger.debug("Keyboard interrupt.")
 
     manager1.stop()
-    manager2.stop()
+    # manager2.stop()
