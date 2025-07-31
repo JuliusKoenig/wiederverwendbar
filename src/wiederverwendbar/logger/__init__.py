e617972d5c56327e7e3ed70f4268d6f893f8fd15
Julius Koenig <julius@koenig-site.de>
2025-07-31 08:06:39 +0200

    Add redirect_level function to manage logger log levels dynamically

--- src/wiederverwendbar/logger/__init__.py
+++ src/wiederverwendbar/logger/__init__.py
@@ -7,5 +7,6 @@ from wiederverwendbar.logger.helper import (logger_exists,
                                             remove_logger)
 from wiederverwendbar.logger.log_levels import (LogLevels)
 from wiederverwendbar.logger.logger import (Logger)
+from wiederverwendbar.logger.redirect_level import (redirect_level)
 from wiederverwendbar.logger.settings import (LoggerSettings)
 from wiederverwendbar.logger.singleton import (LoggerSingleton)
