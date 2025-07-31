a4a36306be6b65e4842ff580bee2ab8eb1e9cf35
Julius Koenig <julius@koenig-site.de>
2025-07-31 13:53:49 +0200

    Add MyAppSettings class to utilize examples module and refactor main function

--- examples/branding.py
+++ examples/branding.py
@@ -1,5 +1,17 @@
 from wiederverwendbar.branding.settings import BrandingSettings
 
+import examples
+
+
+class MyAppSettings(BrandingSettings, module=examples):
+    attr1: str
+    attr2: str
+
+
+def main():
+    app_settings = MyAppSettings(attr1="value1", attr2="value2")
+    print(app_settings.model_dump_json(indent=2))
+
+
 if __name__ == '__main__':
-    branding_settings = BrandingSettings()
-    print(branding_settings.model_dump_json(indent=2))
+    main()
