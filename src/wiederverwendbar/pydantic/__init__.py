27fb080640839d5b40c6f1514ee5ef57e2851f36
Julius Koenig <julius@koenig-site.de>
2025-08-15 16:36:40 +0200

    Rollback FileConfig class

--- src/wiederverwendbar/pydantic/__init__.py
+++ src/wiederverwendbar/pydantic/__init__.py
@@ -1,12 +1,6 @@
 from wiederverwendbar.pydantic.security import (HashedPasswordModel)
 from wiederverwendbar.pydantic.types import (Version)
-from wiederverwendbar.pydantic.file_config import (register_loader,
-                                                   register_saver,
-                                                   FileConfig,
-                                                   FileConfigError,
-                                                   FileConfigLoadingError,
-                                                   FileConfigSavingError,
-                                                   JsonFileConfig)
+from wiederverwendbar.pydantic.file_config import (FileConfig)
 from wiederverwendbar.pydantic.indexable_model import (IndexableModel)
 from wiederverwendbar.pydantic.printable_settings import (PrintableSettings)
 from wiederverwendbar.pydantic.singleton import (ModelSingleton)
