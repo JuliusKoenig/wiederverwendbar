7b91adeb2da4303aaf56d2a73ccafa55e1fd3461
Julius Koenig <julius@koenig-site.de>
2025-10-15 10:42:32 +0200

    Enhance Version class to support prerelease types and numbers

--- examples/pydantic_examples/version.py
+++ examples/pydantic_examples/version.py
@@ -1,20 +1,29 @@
 from wiederverwendbar.pydantic import Version
 
 if __name__ == "__main__":
-    version1_str = "1.0.0"
+    version1_str = "1.0.0a1"
     version2_major = 1
     version2_minor = 0
-    version2_patch = 1
-    version3_str = "v1.0.0"
+    version2_patch = 0
+    version2_prerelease_type = "release_candidate"
+    version2_prerelease_number = 1
+    version3_str = "v1.0.0a1"
 
 
     version1 = Version(version1_str)
-    version2 = Version(major=version2_major, minor=version2_minor, patch=version2_patch)
+    version2 = Version(major=version2_major,
+                       minor=version2_minor,
+                       patch=version2_patch,
+                       prerelease_type=version2_prerelease_type,
+                       prerelease_number=version2_prerelease_number)
     version3 = Version(version3_str)
 
-    print(int(version1))
-    print(int(version2))
-    print(int(version3))
+    version1_int = int(version1)
+    version2_int = int(version2)
+    version3_int = int(version3)
+    print(version1_int)
+    print(version2_int)
+    print(version3_int)
 
     print(version1 == version2)  # False
     print(version1 != version2)  # True
