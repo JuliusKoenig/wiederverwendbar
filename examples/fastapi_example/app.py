37e824c99cdf95f7fc54a1fd1a56d3f8748f79f2
Julius Koenig <info@bastelquartier.de>
2026-01-06 16:04:57 +0100

    wip

--- examples/fastapi_example/app.py
+++ examples/fastapi_example/app.py
@@ -1,3 +1,7 @@
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.routing import Route
+
 from wiederverwendbar.fastapi.root.app import RootApp
 from wiederverwendbar.logger import LoggerSingleton
 from wiederverwendbar.uvicorn import UvicornServer
@@ -7,8 +11,33 @@ from examples.fastapi_example.api import api
 
 LoggerSingleton(name=__name__, settings=settings.logger, init=True)  # ToDo fix this unresolved attr
 
+html = """<!DOCTYPE html>
+<html>
+<head>
+    <title>FastAPI Example</title>
+</head>
+<body>
+    <h1>Welcome to the FastAPI Example Application!</h1>
+    <p>This is a simple example of a FastAPI application with mounted Starlette apps.</p>
+    <a href="/">Go to Root and test auto redirect.</a><br>
+    <a href="/ui">Go to UI.</a><br>
+    <a href="/api">Go to API.</a><br
+</body>
+</html>
+"""
+
+ui = Starlette(
+    routes=[
+        Route(
+            "/",
+            lambda r: HTMLResponse(html),
+        ),
+    ],
+)
+
 app = RootApp(settings=settings.root)
 
+app.mount(path="/ui", app=ui, name="ui")
 app.mount(path="/api", app=api, name="api")
 
 
--- src/wiederverwendbar/fastapi/api/settings.py
+++ src/wiederverwendbar/fastapi/api/settings.py
@@ -9,7 +9,6 @@ from wiederverwendbar.pydantic.types.version import Version
 
 
 class ApiAppSettings(RootAppSettings):
-    # debug: Union[Default, bool] = Field(default=Default(), title="FastAPI Debug", description="Whether the FastAPI is in debug mode.")
     title: Union[Default, str] = Field(default=Default(), title="FastAPI Title", description="The title of the FastAPI.")
     summary: Union[None, Default, str] = Field(default=Default(), title="FastAPI Summary", description="The summary of the FastAPI.")
     description: Union[Default, str] = Field(default=Default(), title="FastAPI Description", description="The description of the FastAPI.")
@@ -26,8 +25,6 @@ class ApiAppSettings(RootAppSettings):
     terms_of_service: Union[None, Default, str] = Field(default=Default(), title="FastAPI Terms of Service", description="The terms of service of the FastAPI.")
     contact: Union[None, Default, dict[str, str]] = Field(default=Default(), title="FastAPI Contact", description="The contact of the FastAPI.")
     license_info: Union[None, Default, dict[str, str]] = Field(default=Default(), title="FastAPI License Info", description="The license info of the FastAPI.")
-    # root_path: Union[Default, str] = Field(default=Default(), title="FastAPI Root Path", description="The root path of the FastAPI.")
-    # root_path_in_servers: Union[Default, bool] = Field(default=Default(), title="FastAPI Root Path in Servers", description="Whether the root path of the FastAPI is in servers.")
     deprecated: Union[None, Default, str] = Field(default=Default(), title="FastAPI Deprecated", description="Whether the FastAPI is deprecated.")
     info_url: Union[None, Default, str] = Field(default=Default(), title="FastAPI Info URL", description="The info URL of the FastAPI.")
     info_tags: Union[Default, list[str]] = Field(default=Default(), title="FastAPI Info tags", description="The info tags for info route in OpenAPI schema.")
